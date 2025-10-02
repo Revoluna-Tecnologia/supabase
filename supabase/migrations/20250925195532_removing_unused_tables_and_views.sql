f8ff8765f19a049f60754435cceb84866f0fe2c6
Éder Monteiro <monteiro.eder@gmail.com>
2025-10-01 23:33:50 -0100

    rename tabelas to snake caase

--- supabase/migrations/20250925195532_removing_unused_tables_and_views.sql
+++ supabase/migrations/20250925195532_removing_unused_tables_and_views.sql
@@ -1,23 +1,23 @@
 -- Remove views
-DROP VIEW IF EXISTS vw_candidaturas_pendentes CASCADE;
-DROP VIEW IF EXISTS vw_candidaturas_por_dia CASCADE;
-DROP VIEW IF EXISTS vw_dashboard_metrics CASCADE;
-DROP VIEW IF EXISTS vw_distribuicao_especialidades CASCADE;
-DROP VIEW IF EXISTS vw_grupo_nome CASCADE;
-DROP VIEW IF EXISTS vw_ocupacao_plantoes CASCADE;
-DROP VIEW IF EXISTS vw_todas_candidaturas CASCADE;
-DROP VIEW IF EXISTS vw_usuarios_por_dia CASCADE;
-DROP VIEW IF EXISTS vw_vagas_dias_contagem CASCADE;
-DROP VIEW IF EXISTS vw_vagas_especialidade CASCADE;
-DROP VIEW IF EXISTS vw_vagas_grade_info CASCADE;
-DROP VIEW IF EXISTS vw_vagas_por_mes CASCADE;
+DROP VIEW IF EXISTS "public"."vw_candidaturas_pendentes" CASCADE;
+DROP VIEW IF EXISTS "public"."vw_candidaturas_por_dia" CASCADE;
+DROP VIEW IF EXISTS "public"."vw_dashboard_metrics" CASCADE;
+DROP VIEW IF EXISTS "public"."vw_distribuicao_especialidades" CASCADE;
+DROP VIEW IF EXISTS "public"."vw_grupo_nome" CASCADE;
+DROP VIEW IF EXISTS "public"."vw_ocupacao_plantoes" CASCADE;
+DROP VIEW IF EXISTS "public"."vw_todas_candidaturas" CASCADE;
+DROP VIEW IF EXISTS "public"."vw_usuarios_por_dia" CASCADE;
+DROP VIEW IF EXISTS "public"."vw_vagas_dias_contagem" CASCADE;
+DROP VIEW IF EXISTS "public"."vw_vagas_especialidade" CASCADE;
+DROP VIEW IF EXISTS "public"."vw_vagas_grade_info" CASCADE;
+DROP VIEW IF EXISTS "public"."vw_vagas_por_mes" CASCADE;
 
 DROP MATERIALIZED VIEW IF EXISTS vw_vagas_disponiveis CASCADE;
 
 -- Remove tables
-DROP TABLE IF EXISTS local_medico CASCADE;
-DROP TABLE IF EXISTS local CASCADE;
-DROP TABLE IF EXISTS sistema_logs CASCADE;
-DROP TABLE IF EXISTS tipos_documentos CASCADE;
-DROP TABLE IF EXISTS validacao_documentos CASCADE;
+DROP TABLE IF EXISTS "public"."local_medico" CASCADE;
+DROP TABLE IF EXISTS "public"."local" CASCADE;
+DROP TABLE IF EXISTS "public"."sistema_logs" CASCADE;
+DROP TABLE IF EXISTS "public"."tipos_documentos" CASCADE;
+DROP TABLE IF EXISTS "public"."validacao_documentos" CASCADE;
 
---
+++ supabase/migrations/20250926000618_rename_tables_snake_case.sql
@@ -0,0 +1,5 @@
+ALTER TABLE IF EXISTS "public"."bannerMKT" RENAME TO banner_mkt;
+ALTER TABLE IF EXISTS "public"."codigosdearea" RENAME TO codigos_de_area;
+ALTER TABLE IF EXISTS "public"."estadosBrasil" RENAME TO estados_brasil;
+ALTER TABLE IF EXISTS "public"."tipovaga" RENAME TO tipo_vaga;
+ALTER TABLE IF EXISTS "public"."whatsappnumber" RENAME TO whatsapp_number;
