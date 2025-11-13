03ad4b260fe1547ad5482c2ecf8c3a007881aad0
Éder Monteiro <monteiro.eder@gmail.com>
2025-11-12 23:10:26 -0100

    fix: seed

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -62,8 +62,8 @@ ON CONFLICT (role, permission) DO NOTHING;
 -- Data for Name: grupos; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO "public"."grupos" ("id", "nome", "created_at", "updated_at", "updated_by") VALUES
-	('ada3a79a-6437-4e27-9e22-40c08c36c59b', 'Grupo Padrão', NOW(), NOW(), 'ada3a79a-6437-4e27-9e22-40c08c36c59b')
+INSERT INTO "public"."grupos" ("id", "nome", "created_at") VALUES
+	('ada3a79a-6437-4e27-9e22-40c08c36c59b', 'Grupo Padrão', NOW())
 ON CONFLICT (id) DO NOTHING;
 
 --
@@ -101,16 +101,14 @@ ON CONFLICT (id) DO NOTHING;
 --
 
 INSERT INTO "public"."escalistas" (
-    "id", "nome", "telefone", "email", "grupo_id", "created_at", "updated_at", "updated_by"
+    "id", "nome", "telefone", "email", "grupo_id", "created_at"
 ) VALUES (
     'ada3a79a-6437-4e27-9e22-40c08c36c59b',
     'Escalista Migrado - Dados Órfãos',
     '(00) 00000-0000',
     'escalista.migrado@placeholder.com',
     'ada3a79a-6437-4e27-9e22-40c08c36c59b',
-    NOW(),
-    NOW(),
-    'ada3a79a-6437-4e27-9e22-40c08c36c59b'
+    NOW()
 )
 ON CONFLICT (id) DO NOTHING;
 
